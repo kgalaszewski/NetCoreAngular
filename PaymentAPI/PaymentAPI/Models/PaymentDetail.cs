37a1b7e852fcf30f0f947633e1df07a258b0e15d
Karol Gałaszewski <kgalaszewski95@gmail.com>
2021-03-30 15:40:09 +0200

    Created the project

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
